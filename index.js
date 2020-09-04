e038bba63697caec52929d15e3e2bc52fc0c5af7
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-09-04 18:24:48 +0200

    Basic express app

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 
-require('dotenv').config();
+dotenv.config({path:".config/config.env"});
+
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("Hello World 😼");
+});
+
 app.listen(port, ()=>{
-    console.log("App listening at http://localhost:${port}");
+    console.log(`App listening at http://localhost:${port}`);
 });
