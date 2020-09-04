ecede402ddf579ab0f5ad2dd315dc3af14ee201f
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-09-04 19:07:36 +0200

    Add Morgan Logger

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
 const app = express();
 
-dotenv.config({path:".config/config.env"});
+dotenv.config({path:"./config/config.env"});
 
+const port = process.env.PORT;
 
-const port = process.env.PORT || 3000;
+if(process.env.NODE_ENV==="development"){
+    app.use(morgan("tiny"));
+}
 
 app.get("/", (req, res) => {
     res.send("Hello World 😼");
