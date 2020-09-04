4e603bb5eef802a1c28963881c16853d0576ced9
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-09-04 18:02:46 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log("App listening at http://localhost:${port}");
+});
